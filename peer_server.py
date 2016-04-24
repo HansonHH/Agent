cc7bc802373982035ea2a3676632f49284e782b6
HansonHH <hxixnhan@gmail.com>
2016-04-24 20:28:10 +0000

    add peer.py

--- peer_server.py
+++ peer_server.py
@@ -12,14 +12,34 @@
 from cyclon.peer import Peer
 import time
 
-print '!'*200
-print 'Peer Server'
 
-peer = Peer(5)
-peer.start()
+#peer = Peer(5)
+#peer.start()
 
 #time.sleep(30)
 print '~'*200
 #peer.join()
 
+
+import SocketServer
+
+class MyUDPHandler(SocketServer.BaseRequestHandler):
+    
+    def handle(self):
+        data = self.request[0].strip()
+        socket = self.request[1]
+        print "{} wrote:".format(self.client_address[0])
+        print data
+        socket.sendto(data.upper(), self.client_address)
+
+if __name__ == "__main__":
+    peer = Peer(5)
+    peer.start()
+    
+    print '!'*200
+    print 'Peer Server'
+    
+    HOST, PORT = "10.0.1.11", 9999
+    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
+    server.serve_forever()
 
